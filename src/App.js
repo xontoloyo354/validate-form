d58ce6d8455fb5705dfb684c2d791cfb0446b886
xontoloyo354 <fahmialqaudy426@Gmail.com>
2022-02-07 11:31:26 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Form />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
